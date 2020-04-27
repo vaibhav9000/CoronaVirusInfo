347876434ba67d14d7e6829006e40885f0e3082e
vaibhav9000 <sharmavaibhav9000@gmail.com>
2020-04-27 18:14:56 +0530

    Updates

---
+++ precache-manifest.b767406b1597c84b8bb07a9fc49e4958.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b8b77e761ee84e47c272ed8b88b67a5",
+    "url": "/CoronaVirusInfo/index.html"
+  },
+  {
+    "revision": "e30f3a52084d7cf664ae",
+    "url": "/CoronaVirusInfo/static/css/main.e7cf7100.chunk.css"
+  },
+  {
+    "revision": "c0ca0de9c459eb46aecb",
+    "url": "/CoronaVirusInfo/static/js/2.94285d22.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/CoronaVirusInfo/static/js/2.94285d22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e30f3a52084d7cf664ae",
+    "url": "/CoronaVirusInfo/static/js/main.3f994710.chunk.js"
+  },
+  {
+    "revision": "fa3d520b140ffe82e925",
+    "url": "/CoronaVirusInfo/static/js/runtime-main.cd18fcd8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/CoronaVirusInfo/static/media/logo.5d5d9eef.svg"
+  }
+]);
